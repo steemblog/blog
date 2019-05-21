2b8f94247e9f7b90ef08d5bc4dbe4d0c98179872
Thinker <mstr.raphael@gmail.com>
2019-05-21 23:27:36 +0800

    update the travis command to build into master branch

--- .travis/_deploy.sh
+++ .travis/_deploy.sh
@@ -3,16 +3,16 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "service" ] && exit 0
 
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USERNAME}"
 
-git clone --depth 1 --branch gh-pages --single-branch https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site
+git clone --depth 1 --branch master --single-branch https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site
 cd site
 cp -r ../public/* ./
 
 NOW=$(date +"%Y-%m-%d %H:%M:%S %z")
 git add --all *
 git commit -m "Site updated: ${NOW}" || true
-git push -q origin gh-pages
+git push -q origin master
