b0f3f0d15d8e59c6fc7616bc0b20b69f45ad8b18
Thinker <mstr.raphael@gmail.com>
2019-06-04 15:21:08 +0800

    add travis_wait explicitly

--- .travis/_build.sh
+++ .travis/_build.sh
@@ -8,12 +8,7 @@ set -e
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USERNAME}"
 
-if [ "${DEBUG}" != "true" ]; then
-  travis_wait 60 pipenv run invoke blog.build-all -h github --production
-else
-  echo "enter debug mode"
-  pipenv run invoke blog.build-all -h github --production
-fi
+pipenv run invoke blog.build-all -h github --production
 
 # if it's cron job, deploy to netlify in the same time
 # [ "${TRAVIS_EVENT_TYPE}" != "cron" ] && exit 0
