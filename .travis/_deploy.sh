0ba8cfbfa1997b9291940a110f64b1e348886978
think-in-universe <mstr.raphael@gmail.com>
2019-08-24 11:01:30 +0800

    add setup branch

--- .travis/_deploy.sh
+++ .travis/_deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "setup" ] && exit 0
 
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USERNAME}"
